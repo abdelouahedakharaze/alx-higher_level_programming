30231b7e893b4e17a74ecdddd30dc01d67816e60
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-10-31 20:17:04 +0100

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord("a") <= ord(i) <= ord("z"):
+            print("{}".format(chr(ord(i) - 32)), end="")
+        else:
+            print("{}".format(i), end="")
