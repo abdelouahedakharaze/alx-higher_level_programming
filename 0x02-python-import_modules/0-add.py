9ff229810f6c70928e311b0e374e0abcd2187e0f
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-02 21:00:19 +0100

    d

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
+
 if __name__ == "__main__":
     a = 1
     b = 2
-
-    from add_0 import add
-
     result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print("{} + {} = {}".format(a, b, result))
