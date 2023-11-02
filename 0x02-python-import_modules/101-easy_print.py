0db25f70691c77bb71337344aebe67e16df0c492
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-02 21:51:11 +0100

    sd

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,17 +1,1 @@
 # /usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    amalia = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(amalia.keys()):
-        print("Unknown operator. Available amalia: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, amalia[sys.argv[2]](a, b)))
