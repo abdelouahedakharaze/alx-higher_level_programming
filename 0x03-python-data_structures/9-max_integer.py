8e79d995b6a6bbfe5721aac0c85f26e9dde92c24
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-06 12:11:51 +0100

    gg

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,11 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    MAxe = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > MAxe:
+            MAxe = my_list[i]
+
+    return MAxe
