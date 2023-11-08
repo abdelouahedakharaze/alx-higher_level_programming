31008ef7f7a34f79ee9c0306180d40a886dcabf7
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-08 15:25:09 +0100

    searchreplece

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    rslt = []
+    for i in my_list:
+        if i == search:
+            rslt.append(replace)
+        else:
+            rslt.append(i)
+    return rslt
