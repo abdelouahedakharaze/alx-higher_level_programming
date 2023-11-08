d39a80cc3120086777bc1d6530895007a02926d7
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-08 16:29:57 +0100

    hope

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,18 +2,20 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    convert_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    rslt = 0
-    valp = 0
 
-    for numeral in roman_string:
-        value = convert_table.get(numeral, 0)
+    rtble = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rls = 0
 
-        if value > valp:
-            rslt += value - 2 * valp
+    for i in range(len(roman_string)):
+        if rtble.get(roman_string[i], 0) == 0:
+            return 0
+
+        if (
+            i != (len(roman_string) - 1)
+            and rtble[roman_string[i]] < rtble[roman_string[i + 1]]
+        ):
+            rls += rtble[roman_string[i]] * -1
+
         else:
-            rslt += value
-
-        valp = value
-
-    return rslt
+            rls += rtble[roman_string[i]]
+    return rls
