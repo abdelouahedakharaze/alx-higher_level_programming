7930872505762fcb8a336111293d7b9ecc406b78
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-08 16:35:01 +0100

    mndi

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,11 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    else:
+        denomin = 0
+        nomin = 0
+        for i in my_list:
+            nomin += i[0] * i[1]
+            denomin += i[1]
+    return nomin / denomin
