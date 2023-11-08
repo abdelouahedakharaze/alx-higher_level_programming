5ecc2836da3af0c42aae078e41fbc0c7c5378864
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-08 16:02:48 +0100

    bestscore

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,7 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    mx = max(a_dictionary.values())
+    for i, j in a_dictionary.items():
+        if j == mx:
+            return i
+    return None
