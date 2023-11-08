4ee344191d35955486e377b06f1fc269ceadf1cb
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-08 16:21:02 +0100

    roma

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    convert_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rslt = 0
+    valp = 0
+
+    for numeral in roman_string:
+        value = convert_table.get(numeral, 0)
+
+        if value > valp:
+            rslt += value - 2 * valp
+        else:
+            rslt += value
+
+        valp = value
+
+    return rslt
