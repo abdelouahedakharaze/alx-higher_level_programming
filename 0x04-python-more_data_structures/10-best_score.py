b74f244354a6a69dd3d74cd03dd84de0d3e97598
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-11-08 16:09:55 +0100

    mbi

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,10 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    mx = max(a_dictionary.values())
-    for i, j in a_dictionary.items():
-        if j == mx:
-            return None
-    return None
+    max_score = max(a_dictionary.values())
+    max_key = None
+    for key, value in a_dictionary.items():
+        if value == max_score:
+            max_key = key
+            break
+    return max_key
