dad54cc6b04e1d825449a7dde8b61ff83a38ed93
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-12-05 12:10:15 +0100

    rty

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
     """Provide a list of attributes accessible for an object."""
-    return (dir(obj))
+    return dir(obj)
