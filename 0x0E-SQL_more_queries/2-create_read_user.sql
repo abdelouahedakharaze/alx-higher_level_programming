09371ac8ae1bc5e3f3abf4eae1a7d62c98fd9a5d
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-01-17 18:37:14 +0100

    try

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,6 @@
 -- Creates the database 'hbtn_0d_2' if it doesn't already exist.
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-
--- Creates the user 'user_0d_2' with a password.
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
 
 
