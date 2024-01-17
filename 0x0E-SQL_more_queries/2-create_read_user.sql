87efe9d13b51244eb5687c57bb60e5a6a1e4dd63
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-01-17 18:32:41 +0100

    s

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,11 +7,4 @@ CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grants SELECT privilege on all tables in 'hbtn_0d_2' to 'user_0d_2'.
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
 
--- Refreshes the privilege tables to apply the changes.
-FLUSH PRIVILEGES;
