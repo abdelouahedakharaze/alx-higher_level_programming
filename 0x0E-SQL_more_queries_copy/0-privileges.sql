8658d5cf66b2f6e25a11613b0a343b38578dc981
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-01-17 18:22:53 +0100

    q

--- 0x0E-SQL_more_queries_copy/0-privileges.sql
+++ 0x0E-SQL_more_queries_copy/0-privileges.sql
@@ -1,5 +1,3 @@
 -- Retrieves the privileges for 'user_0d_1' on the localhost.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Displays the privileges assigned to 'user_0d_2' on the localhost.
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
