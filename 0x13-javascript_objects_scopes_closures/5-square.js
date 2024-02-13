4060be34621233b6fae0b9c9658fce9613653da4
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:37:15 +0100

    mni

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// Abdelouahed Akharaze
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    // Call the constructor of Rectangle with size as both width and height
+    super(size, size);
+  }
+}
+
+module.exports = Square;
