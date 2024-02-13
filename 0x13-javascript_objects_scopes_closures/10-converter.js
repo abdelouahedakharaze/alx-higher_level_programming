c26af9c3fa737735b2b2cecd57e4c19226add614
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:50:12 +0100

    vgbbbg

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// Define the function converter which takes a base as an argument
+exports.converter = function (base) {
+  // Return a function that converts a number from base 10 to the specified base
+  return function (number) {
+    // Use the toString method to convert the number to the specified base
+    return number.toString(base);
+  };
+};
