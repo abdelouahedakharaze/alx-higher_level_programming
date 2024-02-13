0135b1894272a486fb9988f4f167ada1d1529544
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:58:11 +0100

    fuuu

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,25 @@
 #!/usr/bin/node
 // Abdelouahed Akharaze
+// Importing the dict object from 101-data.js
+const dict = require('./101-data').dict;
+
+// Initializing an empty object to store the new dictionary
+const occurrencesById = {};
+
+// Looping through the keys (user ids) of the original dictionary
+for (const userId in dict) {
+  // Getting the number of occurrences for the current user id
+  const occurrences = dict[userId];
+
+  // Checking if the occurrences value is already a key in the new dictionary
+  if (!occurrencesById[occurrences]) {
+    // If not, initialize it with an empty array
+    occurrencesById[occurrences] = [];
+  }
+
+  // Pushing the user id to the list of user ids for the current number of occurrences
+  occurrencesById[occurrences].push(userId);
+}
+
+// Printing the new dictionary
+console.log(occurrencesById);
