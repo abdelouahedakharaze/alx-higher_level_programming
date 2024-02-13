b656255f96231e3ea043cd338f6fa6f1a94c2559
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:48:12 +0100

    bvnnju

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// Define the function esrever which takes a list as an argument
+exports.esrever = function (list) {
+  // Initialize an empty array to store the reversed list
+  const reversedList = [];
+
+  // Iterate through the original list in reverse order
+  for (let i = list.length - 1; i >= 0; i--) {
+    // Push each element of the original list into the reversed list
+    reversedList.push(list[i]);
+  }
+
+  // Return the reversed list
+  return reversedList;
+};
