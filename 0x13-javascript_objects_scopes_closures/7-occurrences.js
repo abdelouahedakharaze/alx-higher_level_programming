756df132f50fe28fdf1046af175abff424cbc70c
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:47:16 +0100

    nbvc

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+// Define the function nbOccurences which takes a list and a searchElement as arguments
+exports.nbOccurences = function (list, searchElement) {
+  // Initialize a variable count to count the occurrences
+  let count = 0;
+
+  // Iterate through the list
+  for (let i = 0; i < list.length; i++) {
+    // If the current element in the list is equal to the searchElement, increment count
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+
+  // Return the count of occurrences
+  return count;
+};
