f06719bb57135405fafb0eed8f1f53e9e339c011
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:39:10 +0100

    ytuuu

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+// Abdelouahed Akharaze
+
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+  constructor (size) {
+    // Call the constructor of SquareParent with size as the argument
+    super(size);
+  }
+
+  charPrint (c) {
+    // If c is undefined, use 'X' as the default character
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    // Print the square using the character c
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
