b0f2e13e468d88bbc9f247ee85d1c07125dff999
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:12:46 +0100

    g

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Abdelouahed Akharaze
+
+// Rectangle class definition
+class Rectangle {
+    // Empty constructor
+    constructor() {}
+}
+
+// Exporting the Rectangle class to make it available for use in other files
+module.exports = Rectangle;
