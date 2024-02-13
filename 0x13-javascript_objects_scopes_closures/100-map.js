3e74b3607464019c139f7c3c6d1497a072ac0bc0
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:55:16 +0100

    nmbb

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Abdelouahed Akharaze
-// Import the list array from 100-data.js
-const { list } = require('./100-data');
+// Importing the list array from 100-data.js
+const list = require('./100-data').list;
 
-// Use the map function to compute a new array where each value is equal to the value of the initial list multiplied by the index in the list
-const newList = list.map((value, index) => value * index);
+// Using the map function to compute a new array where each value is equal to the value of the initial list multiplied by its index
+const newList = list.map((el, idx) => el * idx);
 
-// Print both the initial list and the new list
+// Printing both the initial list and the new list
 console.log(list);
 console.log(newList);
