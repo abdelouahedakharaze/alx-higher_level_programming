71bb2c55efdde1b9f49b31571cc6684e508a8535
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:46:15 +0100

    huiii

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,21 +2,20 @@
 // Abdelouahed Akharaze
 
 // Importing the Square class from 5-square.js
+// Defining a subclass Square1 that extends the Square class
+// Defining a method charPrint that prints the square using the character c
+// If c is not provided, call the print method from the parent class
+// Otherwise, print the character c repeated height times
+// Exporting the Square1 class for use in other files
 const Square = require('./5-square');
 
-// Defining a subclass Square1 that extends the Square class
 class Square1 extends Square {
-  // Defining a method charPrint that prints the square using the character c
   charPrint (c) {
-    // If c is not provided, call the print method from the parent class
-    if (!c) { this.print(); }
-
-    // Otherwise, print the character c repeated height times
+    if (!c) this.print();
     else {
-      for (let i = 0; i < this.width; i++) { console.log(c.repeat(this.height)); }
+      for (let i = 0; i < this.width; i++) console.log(c.repeat(this.height));
     }
   }
 }
 
-// Exporting the Square1 class for use in other files
 module.exports = Square1;
