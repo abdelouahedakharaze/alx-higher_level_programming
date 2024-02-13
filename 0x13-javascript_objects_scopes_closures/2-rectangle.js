5e0ad41df7e3b48d746d4dba0fb68ccf824aaa1e
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:34:04 +0100

    jgh

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,13 +5,11 @@
 class Rectangle {
   constructor (w, h) {
     // Check if both width (w) and height (h) are positive integers
+
     if (w > 0 && h > 0) {
       // If so, initialize width and height attributes
       this.width = w;
       this.height = h;
-    } else {
-      // If either w or h is not a positive integer, return an empty object
-      return {};
     }
   }
 }
