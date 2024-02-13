dfc2f08a0041c333f7e70742543f6a2c3bb0a01d
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:14:49 +0100

    h

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+// Abdelouahed Akharaze
+
+// Rectangle class definition
+class Rectangle {
+    // Constructor with two arguments: w (width) and h (height)
+    constructor(w, h) {
+        // Initialize instance attribute width with the value of w
+        this.width = w;
+        // Initialize instance attribute height with the value of h
+        this.height = h;
+    }
+}
+
+// Exporting the Rectangle class to make it available for use in other files
+module.exports = Rectangle;
