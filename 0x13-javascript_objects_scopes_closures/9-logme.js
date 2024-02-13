5ae8e95928f4ef9e32b1b9f7c238b2dad06d8744
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-13 10:49:21 +0100

    vcgyu

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// Define the function logMe which takes an item as an argument
+exports.logMe = function (item) {
+  // Define a variable to keep track of the number of arguments already printed
+  if (this.count === undefined) {
+    this.count = 0;
+  }
+
+  // Print the number of arguments already printed and the current argument value
+  console.log(this.count + ': ' + item);
+
+  // Increment the count of arguments already printed
+  this.count++;
+};
