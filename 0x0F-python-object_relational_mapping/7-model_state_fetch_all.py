05d4c4ccc44fce9ed62bd32511220ba376361539
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-17 21:08:37 +0100

    sd

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,7 +14,10 @@ if __name__ == "__main__":
     Access the database and retrieve the states.
     """
 
-    db_uri = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
+    db_uri = "mysql+mysqldb://{}:{}@localhost\
+    :3306/{}".format(
+        argv[1], argv[2], argv[3]
+    )
     engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
 
